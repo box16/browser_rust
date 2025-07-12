d7fdb38d2adadc639a6d545ff30117dcb7364181
box16 <inoinobox16@gmail.com>
2025-07-12 13:22:46 +0900

    port指定した場合の対応

--- browser_core/src/url.rs
+++ browser_core/src/url.rs
@@ -1,4 +1,7 @@
-use alloc::string::{String, ToString};
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 
 #[derive(PartialEq, Debug)]
 pub struct URL {
@@ -11,10 +14,18 @@ pub struct URL {
 
 impl URL {
     pub fn new(url: String) -> Self {
+        let host = url.trim_start_matches("http://");
+        let host_parts: Vec<&str> = host.splitn(2, ":").collect();
+        let port = if host_parts.len() == 2 {
+            host_parts[1].to_string()
+        } else {
+            "80".to_string()
+        };
+
         Self {
             scheme: "http".to_string(),
             host: "example.com".to_string(),
-            port: "80".to_string(),
+            port: port,
             path: "".to_string(),
             search_part: "".to_string(),
         }
@@ -32,6 +43,19 @@ mod tests {
             scheme: "http".to_string(),
             host: "example.com".to_string(),
             port: "80".to_string(),
+            path: "".to_string(),
+            search_part: "".to_string(),
+        };
+        assert_eq!(expected, URL::new(url));
+    }
+
+    #[test]
+    fn parse_port_specified_http_url() {
+        let url = "http://example.com:8080".to_string();
+        let expected = URL {
+            scheme: "http".to_string(),
+            host: "example.com".to_string(),
+            port: "8080".to_string(),
             path: "".to_string(),
             search_part: "".to_string(),
         };
